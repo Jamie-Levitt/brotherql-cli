534349a1c4d805cdf802c07c8a71fdbc55480af3
Jamie-Levitt <157087206+Jamie-Levitt@users.noreply.github.com>
2025-07-24 20:59:28 +1000

    commit

--- brotherql_cli/cli/print.py
+++ brotherql_cli/cli/print.py
@@ -7,15 +7,15 @@ import typer
 print_app = typer.Typer(cls=ErrorHandlerTyper, pretty_exceptions_enable=False)
 
 @print_app.command("print", help="Print list of strings to label")
-def print_label(label_lines:List[str]):
+def print_label(label_lines:str):
     try:
         # print("[DEBUG] Cleaning lines")
-        label_lines = [line.strip() for line in label_lines
-                        if line.strip() != ""]
-        if len(label_lines) == 0:
+        lines = [line.strip() for line in label_lines.split(";")
+                    if line.strip() != ""]
+        if len(lines) == 0:
             raise ValueError(f"Argument label_lines cannot be empty, or full of empty strings. [{label_lines}]")
     
         # print("[DEBUG] Sending to print")
-        print_from_lines(label_lines)
+        print_from_lines(lines)
     except Exception as e:
         raise PrintException(e)
